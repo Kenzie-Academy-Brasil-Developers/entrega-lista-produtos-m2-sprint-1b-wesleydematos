d778f95c56e108b46275babbf156170eb0c65e63
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-07-28 09:08:47 -0300

    create function listarProdutos

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,29 @@
-// seu código aqui
+function listarProdutos(arrayDeProdutos){
+    let ulProdutos = document.querySelector("ul")
+
+    for(let i = 0; i<arrayDeProdutos.length; i++){
+        let produto             = arrayDeProdutos[i]
+        let cardProduto         = criarCardProduto(produto)
+
+        ulProdutos.appendChild(cardProduto)
+    }
+}
+listarProdutos(produtos)
+
+function criarCardProduto(produto){
+    let tagLi   = document.createElement("li")
+    let tagImg  = document.createElement("img")
+    let tagH3   = document.createElement("h3")
+    let tagSpan = document.createElement("span")
+    let tagP    = document.createElement("p")
+
+    tagImg.src        = produto.img
+    tagImg.alt        = `Imagem ${produto.nome}`
+    tagH3.innerText   = produto.nome
+    tagSpan.innerText = produto.secao
+    tagP.innerText    = `R$ ${produto.preco}.00`
+
+    tagLi.append(tagImg, tagH3, tagSpan, tagP)
+
+    return tagLi
+}
