c4e651da9e2198e28ebdba7d0d91d2091a0ecc51
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-07-28 19:46:01 -0300

    final commit

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,15 @@ let ulProdutos = document.querySelector("ul")
 
 function listarProdutos(arrayDeProdutos){
 
-    for(let i = 0; i<arrayDeProdutos.length; i++){
-        let produto             = arrayDeProdutos[i]
-        let cardProduto         = criarCardProduto(produto)
-
-        ulProdutos.appendChild(cardProduto)
+    if(arrayDeProdutos == undefined){
+        ulProdutos.innerHTML = `<div class="novaBusca">Liste todos os produtos novamente e faça uma nova busca!</div>`
+    }else{
+        for(let i = 0; i<arrayDeProdutos.length; i++){
+            let produto             = arrayDeProdutos[i]
+            let cardProduto         = criarCardProduto(produto)
+    
+            ulProdutos.appendChild(cardProduto)
+        }
     }
 }
 listarProdutos(produtos)
@@ -40,4 +44,46 @@ function calcularTotal(array){
     }
 
     document.querySelector(".totalPreco").innerText = `R$ ${total}.00`
+}
+
+let inputBusca = document.querySelector(".containerBuscaPorNome input")
+let btnBusca = document.querySelector(".containerBuscaPorNome button")
+
+btnBusca.addEventListener("click", function(event){
+    let btnBuscar  = event.target
+    
+    if(btnBuscar.tagName == "BUTTON"){
+        let pesquisaUsuario = inputBusca.value
+        let resultadoBusca  = busca(pesquisaUsuario)
+
+        ulProdutos.innerHTML = ''
+        if(resultadoBusca == undefined){
+            inputBusca.value = ""
+        }
+        listarProdutos(resultadoBusca)
+        inputBusca.value = ""
+    }
+})
+
+function busca(valorPesquisa){
+    
+    let resultBusca = []
+
+    for(let i = 0; i < produtos.length; i++){
+        
+        let pesquisa    = valorPesquisa.toLowerCase()
+        let nomeProduto = produtos[i].nome.toLowerCase()
+        let categoria   = produtos[i].secao.toLowerCase()
+        
+        if(nomeProduto.includes(pesquisa) || categoria.includes(pesquisa)){
+            resultBusca.push(produtos[i])
+        }
+    }
+
+    if(resultBusca.length > 0){
+        calcularTotal(resultBusca)
+        return resultBusca
+    } 
+    document.querySelector(".totalPreco").innerText = `R$ 00.00`
+    alert("Busca não encontrada!")
 }
